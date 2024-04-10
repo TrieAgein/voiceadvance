c1af9a856879d475abfc7326f189fe8483b6733a
lascencio27 <148408677+lascencio27@users.noreply.github.com>
2024-04-10 16:25:00 -0700

    Created a component for the dropdown

---
+++ Dropdown.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import arrowsSVG from '/public/arrows.svg';
+import '../css/page.css';
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <a onClick={toggleDropdown} className="title">Comments
+        <Image
+              src={arrowsSVG}
+              height="0"
+              width="20"
+              float="right"
+              vertical-align="middle"
+              rotate="180deg"
+              alt='arrow'
+        /> 
+      </a>
+      {isOpen && (
+        <div className="content">
+          <a>All</a>
+          <a>Active</a>
+          <a>Manage</a> 
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
