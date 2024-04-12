ac815682d21102b4bd531a462d43a52076bffc60
Haris Khan <khanharis2122@gmail.com>
2024-04-11 20:59:21 -0700

    developed template for resolver page

---
+++ app/resolver.js
@@ -0,0 +1,76 @@
+// Import TestDbComponent or any other components if needed
+export default function Resolver() {
+    // You can define more functions for other buttons similarly
+  
+    return (
+      <div>
+      <header>
+        <div className="title">
+          {/* Assuming you want to use Image component but have it commented out */}
+          <Image
+              src={logo}
+              width="50"
+              margin="5"
+              alt="logo"
+            />
+          VoiceAdvance
+        </div>
+        <div className="account-tab">
+          <div className="profile-pic">
+            <Image
+              src={avatar}
+              display ="inline"
+              margin= "0 auto"
+              margin-left ="-25%"
+              width='100'
+              height='0'
+              alt="profile"
+            />
+          </div>
+          <div className="account-name">Resolver</div>
+        </div>
+      </header>
+      
+      <content>
+        <div className="operation-bar">
+          <div style={{ display: 'inline', float: 'right' }}>
+            <a className="operation-button" onClick={toggleView}>Toggle View</a>{' '}
+            <a className="operation-button">Search</a>{' '}
+            <input className="search" placeholder="Search" type="text"/>{' '}
+            <a className="button">New Claim <span style={{ fontSize: '150%' }}>+</span></a>
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+          <div className="sidebar">
+              <Dropdown/>
+              <Dropdown/>
+              <Dropdown/>
+            <div className="personal-statistics">
+              <p>Resolver Statistics</p>
+              <hr />
+              <div className="wrapper">
+                <div className="stat">
+                <center id="stat-resolvedcount">0</center>
+                  Resolved
+                </div>
+                <div className="stat">
+                <center id="stat-upvotecount">0</center>
+                  Unresolved
+                </div>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+        <div id="claim-list" className="claim-list line">
+          <CommentsList/>
+        </div>
+        <div>
+          <CommentSubmitForm/>
+        </div>
+      </content>
+      
+      <footer className="footer"></footer>
+    </div>
+    );
+  }
