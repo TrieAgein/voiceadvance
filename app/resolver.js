f8b602a95eee26b89b764b33d4257a7c28a21aa3
Haris Khan <khanharis2122@gmail.com>
2024-04-17 22:29:01 -0700

    Implemented commentbox component into resolver page

--- app/resolver.js
+++ app/resolver.js
@@ -1,76 +1,96 @@
+"use client";
+import Image from "next/image";
+import "./css/page.css";
+import React from 'react';
+import toggleView from './components/toggleView';
+import CommentsList from './components/commentsList.client.js';
+import CreateUser from './components/createUser.js';
+import Dropdown from './components/dropdown.js';
+import logo from './public/logo.svg';
+import avatar from './public/avatar.jpg';
+import CommentBox from "./components/commentBox.js";
+
+
 // Import TestDbComponent or any other components if needed
 export default function Resolver() {
-    // You can define more functions for other buttons similarly
-  
-    return (
-      <div>
-      <header>
-        <div className="title">
-          {/* Assuming you want to use Image component but have it commented out */}
+  // You can define more functions for other buttons similarly
+
+
+  return (
+    <div>
+    <header>
+      <div className="title">
+        {/* Assuming you want to use Image component but have it commented out */}
+        <Image
+            src={logo}
+            width="50"
+            margin="5"
+            alt="logo"
+          />
+        VoiceAdvance
+      </div>
+      <div className="account-tab">
+        <div className="profile-pic">
           <Image
-              src={logo}
-              width="50"
-              margin="5"
-              alt="logo"
-            />
-          VoiceAdvance
+            src={avatar}
+            display ="inline"
+            margin= "0 auto"
+            margin-left ="-25%"
+            width='100'
+            height='0'
+            alt="profile"
+          />
         </div>
-        <div className="account-tab">
-          <div className="profile-pic">
-            <Image
-              src={avatar}
-              display ="inline"
-              margin= "0 auto"
-              margin-left ="-25%"
-              width='100'
-              height='0'
-              alt="profile"
-            />
-          </div>
-          <div className="account-name">Resolver</div>
+        <div className="account-name">Julien</div>
+      </div>
+    </header>
+   
+    <content>
+      <div className="operation-bar">
+        <div style={{ display: 'inline', float: 'right' }}>
+          <a className="operation-button" onClick={toggleView}>Toggle View</a>{' '}
+          <a className="operation-button">Search</a>{' '}
+          <input className="search" placeholder="Search" type="text"/>{' '}
+          <a className="button">New Claim <span style={{ fontSize: '150%' }}>+</span></a>
         </div>
-      </header>
-      
-      <content>
-        <div className="operation-bar">
-          <div style={{ display: 'inline', float: 'right' }}>
-            <a className="operation-button" onClick={toggleView}>Toggle View</a>{' '}
-            <a className="operation-button">Search</a>{' '}
-            <input className="search" placeholder="Search" type="text"/>{' '}
-            <a className="button">New Claim <span style={{ fontSize: '150%' }}>+</span></a>
-          </div>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-          <div className="sidebar">
-              <Dropdown/>
-              <Dropdown/>
-              <Dropdown/>
-            <div className="personal-statistics">
-              <p>Resolver Statistics</p>
-              <hr />
-              <div className="wrapper">
-                <div className="stat">
-                <center id="stat-resolvedcount">0</center>
-                  Resolved
-                </div>
-                <div className="stat">
-                <center id="stat-upvotecount">0</center>
-                  Unresolved
-                </div>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        <div className="sidebar">
+            <Dropdown/>
+          <div className="personal-statistics">
+            <p>Personal Statistics</p>
+            <hr />
+            <div className="wrapper">
+              <div className="stat">
+              <center id="stat-postcount">0</center>
+                Posts
               </div>
-              <hr />
+              <div className="stat">
+              <center id="stat-resolvedcount">0</center>
+                Resolved
+              </div>
+              <div className="stat">
+              <center id="stat-upvotecount">0</center>
+                Upvotes
+              </div>
             </div>
+            <hr />
           </div>
         </div>
-        <div id="claim-list" className="claim-list line">
-          <CommentsList/>
-        </div>
-        <div>
-          <CommentSubmitForm/>
-        </div>
-      </content>
-      
-      <footer className="footer"></footer>
-    </div>
-    );
-  }
+      </div>
+      <div id="claim-list" className="claim-list list">
+      <CommentBox/>
+      </div>
+      <div>    
+        {/* Insert multiple comments. Styling still in development*/}
+        <CommentsList/>
+      </div>
+      <div>
+        <CreateUser/>
+      </div>
+    </content>
+   
+    <footer className="footer"></footer>
+  </div>
+  );
+}
